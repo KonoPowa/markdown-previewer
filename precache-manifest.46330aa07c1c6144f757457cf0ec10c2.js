3608e3696838bce5bc2f846d347af463f3b326cc
Yacob Abusadah <yacob.abusadah@gmail.com>
2020-07-30 00:33:12 -0400

    Updates

---
+++ precache-manifest.46330aa07c1c6144f757457cf0ec10c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38797fbf4620ab78e6e11afd1aefb0a0",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "df52c40703dca85ed790",
+    "url": "/markdown-previewer/static/css/main.6f59cb07.chunk.css"
+  },
+  {
+    "revision": "b952b101651d8b7d7f4b",
+    "url": "/markdown-previewer/static/js/2.afc27a16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.afc27a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df52c40703dca85ed790",
+    "url": "/markdown-previewer/static/js/main.f256d674.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
